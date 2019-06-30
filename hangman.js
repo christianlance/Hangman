9e6bfd41d52fbfb71d0ee59616411193c3ea829e
christianlance <50809805+christianlance@users.noreply.github.com>
2019-06-30 18:16:48 -0400

    ...

--- hangman.js
+++ hangman.js
@@ -2,7 +2,7 @@ var easybtn = document.getElementById("easybtn");
 
 easybtn.addEventListener("click", function(){
     var easyRequest = new XMLHttpRequest();
-    easyRequest.open('GET', 'https://hangman-micro-service-bpblrjerwh.now.sh/?difficulty=easy');
+    easyRequest.open('GET', 'https://hangman-micro-service-bpblrjerwh.now.sh/?difficulty=easy')
     easyRequest.onLoad = function(){
         var easyData = JSON.parse(easyRequest.responseText);
         console.log(easyData[0]);
