6d3e5a3fd8377ad4246a74f38535cb2b77f2c2af
christianlance <50809805+christianlance@users.noreply.github.com>
2019-06-30 19:08:42 -0400

    more button functions

--- hangman.js
+++ hangman.js
@@ -9,51 +9,25 @@ easybtn.addEventListener("click", function(){
     }; 
     easyRequest.send();
 });
-(function () {
-    "use strict";
-    var availableLetters, words, guessInput, guess, guessButton, lettersGuessed, lettersMatched, output, man, letters, lives, currentWord, numLettersMatched, messages;
+var mediumbtn = document.getElementById("mediumbtn");
 
-    function setup() {
-        /* start config options */
-        availableLetters = "abcdefghijklmnopqrstuvwxyz";
-        lives = 5;
-        words = [easyData];
-        messages = {
-            win: 'You win!',
-            lose: 'Game over!',
-            guessed: ' already guessed, please try again...',
-            validLetter: 'Please enter a letter from A-Z'
-        };
-        /* end config options */
+mediumbtn.addEventListener("click", function(){
+    var mediumRequest = new XMLHttpRequest();
+    mediumRequest.open('GET', 'https://hangman-micro-service-bpblrjerwh.now.sh/?difficulty=easy')
+    mediumRequest.onload = function(){
+        var mediumData = JSON.parse(mediumRequest.responseText);
+        console.log(mediumData);
+    }; 
+    mediumRequest.send();
+});
+var hardbtn = document.getElementById("hardbtn");
 
-        lettersGuessed = lettersMatched = '';
-        numLettersMatched = 0;
-
-        /* choose a word */
-        currentWord = words[Math.floor(Math.random() * words.length)];
-
-        /* make #man and #output blank, create vars for later access */
-        output = document.getElementById("output");
-        man = document.getElementById("man");
-        guessInput = document.getElementById("letter");
-
-        man.innerHTML = 'You have ' + lives + ' lives remaining';
-        output.innerHTML = '';
-
-        document.getElementById("letter").value = '';
-
-        /* make sure guess button is enabled */
-        guessButton = document.getElementById("guess");
-        guessInput.style.display = 'inline';
-        guessButton.style.display = 'inline';
-
-        /* set up display of letters in current word */
-        letters = document.getElementById("letters");
-        letters.innerHTML = '<li class="current-word">Current word:</li>';
-
-        var letter, i;
-        for (i = 0; i < currentWord.length; i++) {
-            letter = '<li class="letter letter' + currentWord.charAt(i).toUpperCase() + '">' + currentWord.charAt(i).toUpperCase() + '</li>';
-            letters.insertAdjacentHTML('beforeend', letter);
-        }
-    }
+hardbtn.addEventListener("click", function(){
+    var hardRequest = new XMLHttpRequest();
+    hardRequest.open('GET', 'https://hangman-micro-service-bpblrjerwh.now.sh/?difficulty=easy')
+    hardRequest.onload = function(){
+        var hardData = JSON.parse(hardRequest.responseText);
+        console.log(hardData);
+    }; 
+    hardRequest.send();
+});
