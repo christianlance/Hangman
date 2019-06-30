8eb5a33732b51f89c6028914a261aa583f41d29c
christianlance <50809805+christianlance@users.noreply.github.com>
2019-06-30 16:41:32 -0400

    json

--- hangman.js
+++ hangman.js
@@ -1,6 +1,7 @@
-var word = new XMLHttpRequest;
-word.open('GET', 'https://hangman-micro-service-bpblrjerwh.now.sh/')
-word.onLoad = function(){
-    console.log(word.responseText);
+var easy = new XMLHttpRequest;
+easy.open('GET', 'https://hangman-micro-service-bpblrjerwh.now.sh/?difficulty=easy')
+easy.onLoad = function(){
+    var easy = JSON.parse(easy.responseText);
+    console.log(easy[1]);
 }; 
-word.send();
+easy.send();
