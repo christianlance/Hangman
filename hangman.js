2a2c5fb2689b56564404d5e1aa5b4cdeac09ed33
christianlance <50809805+christianlance@users.noreply.github.com>
2019-06-30 16:48:03 -0400

    fix

--- hangman.js
+++ hangman.js
@@ -1,7 +1,7 @@
 var easy = new XMLHttpRequest;
 easy.open('GET', 'https://hangman-micro-service-bpblrjerwh.now.sh/?difficulty=easy')
 easy.onLoad = function(){
-    var easy = JSON.parse(easy.responseText);
-    console.log(easy[1]);
+    var easyData = JSON.parse(easy.responseText);
+    console.log(easyData[1]);
 }; 
 easy.send();
