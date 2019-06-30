bf106e0d549e16b71effbc2fa7aa8898ed5f969e
christianlance <50809805+christianlance@users.noreply.github.com>
2019-06-30 17:02:33 -0400

    button mod

--- hangman.js
+++ hangman.js
@@ -1,7 +1,11 @@
-var easy = new XMLHttpRequest;
-easy.open('GET', 'https://hangman-micro-service-bpblrjerwh.now.sh/?difficulty=easy')
-easy.onLoad = function(){
-    var easyData = JSON.parse(easy.responseText);
-    console.log(easyData[0]);
-}; 
-easy.send();
+var easybtn = documment.getElementbyID("easybtn")
+easybtn.addEventListener("click", function(){
+    var easy = new XMLHttpRequest;
+    easy.open('GET', 'https://hangman-micro-service-bpblrjerwh.now.sh/?difficulty=easy')
+    easy.onLoad = function(){
+        var easyData = JSON.parse(easy.responseText);
+        console.log(easyData[0]);
+    }; 
+    easy.send();
+});
+
