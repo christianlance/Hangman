c6c8f496ea7e7e1399617ae1b6601d1012ee2f99
christianlance <50809805+christianlance@users.noreply.github.com>
2019-06-30 19:10:36 -0400

    oops

--- hangman.js
+++ hangman.js
@@ -13,7 +13,7 @@ var mediumbtn = document.getElementById("mediumbtn");
 
 mediumbtn.addEventListener("click", function(){
     var mediumRequest = new XMLHttpRequest();
-    mediumRequest.open('GET', 'https://hangman-micro-service-bpblrjerwh.now.sh/?difficulty=easy')
+    mediumRequest.open('GET', 'https://hangman-micro-service-bpblrjerwh.now.sh/?difficulty=medium')
     mediumRequest.onload = function(){
         var mediumData = JSON.parse(mediumRequest.responseText);
         console.log(mediumData);
@@ -24,7 +24,7 @@ var hardbtn = document.getElementById("hardbtn");
 
 hardbtn.addEventListener("click", function(){
     var hardRequest = new XMLHttpRequest();
-    hardRequest.open('GET', 'https://hangman-micro-service-bpblrjerwh.now.sh/?difficulty=easy')
+    hardRequest.open('GET', 'https://hangman-micro-service-bpblrjerwh.now.sh/?difficulty=hard')
     hardRequest.onload = function(){
         var hardData = JSON.parse(hardRequest.responseText);
         console.log(hardData);
